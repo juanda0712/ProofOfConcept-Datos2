e978e72ff466f11889dcca4e0164bc6e4e4484d7
Isaac Herrera Monge <Isaacoun100@gmail.com>
2021-11-11 22:02:50 -0600

    App new course added
    Coursed may now be added

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
+import NewCourse from './NewCourse' ;
 
 function App() {
   return (
@@ -9,9 +10,15 @@ function App() {
         <Navbar></Navbar>
         <div className="content">
           <Switch>
+
             <Route exact path="/">
               <Home></Home>
             </Route>
+
+            <Route path="/new-course">
+              <NewCourse></NewCourse>
+            </Route>
+
           </Switch>
         </div>
       </div>
